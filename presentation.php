37aa53b9bd97c0f0b1be70406a2fd259c61678c6
RobinCornec <robin.cornec@epsi.fr>
2016-11-01 15:28:33 +0100

    ajout texte onglet "présentation"

--- presentation.php
+++ presentation.php
@@ -5,6 +5,28 @@ include 'header.php';
     <h1>Présentation</h1>
     <hr>
 
+    <h5>Rdc :</h5>
+    <ul>
+        <li>Pièce principale, cuisine équipée (four micro-ondes, lave-vaisselle, plaques vitrocéramiques)</li>
+        <li>Chambre avec 1 lit 2 personnes (140)</li>
+        <li>1 WC</li>
+        <li>1 sdb avec baignoire</li>
+    </ul>
+    <h5>Étage :</h5>
+    <ul>
+        <li>Mezzanine avec un canapé convertible (140) + TV (TNT)</li>
+        <li>Chambre avec 1 lit 2 personnes (160), transformable en 2 lits 1 personne</li>
+        <li>1 salle d'eau avec douche et WC</li>
+    </ul>
+    <h5>Extérieur :</h5>
+    <ul>
+        <li>Table et chaises</li>
+        <li>2 transats</li>
+        <li>barbecue et plancha</li>
+        <li>voile d'ombrage</li>
+    </ul>
+
+    <hr>
     <img class="img-rounded" src=".\public\images\terasse1.JPG" alt="Terasse">
     <img class="img-rounded" src=".\public\images\terasse2.JPG" alt="Terasse">
     <img class="img-rounded" src=".\public\images\sejour.JPG" alt="Séjour">
